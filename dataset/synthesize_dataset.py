d0691c6e121bdca7eacc108bc1862a84d406b73f
Yash Bhamare <mc240041040@iiti.ac.in>
2025-10-02 21:39:02 +0000

    resolved push to huggingface issues

--- dataset/synthesize_dataset.py
+++ dataset/synthesize_dataset.py
@@ -51,6 +51,7 @@ class YoloModify:
 
     def modify_mask(self, images: torch.Tensor, masks :torch.Tensor, size: tuple, mask_padding :int, class_label = 18.):
         new_masks = []
+        new_images = []
         image = images
         mask = masks
         results = self.model(image, verbose = False)
@@ -65,7 +66,7 @@ class YoloModify:
                     yolo_mask[:, int(box_coors[1]):int(box_coors[3]), int(box_coors[0]):int(box_coors[2])] = 11.
                     yolo_mask = self.grow_mask(yolo_mask, mask_padding)   
                     cur_mask = mask[idx]
-                    cur_mask[(cur_mask == 11. / 255.) & (yolo_mask == 0.)] = class_label // 255.
+                    cur_mask[(cur_mask == 11. / 255.) & (yolo_mask == 0.)] = class_label / 255.
                     w = size[0][idx]
                     h = size[1][idx]
                     cur_mask = cur_mask.unsqueeze(0)
@@ -76,31 +77,42 @@ class YoloModify:
                         mode = 'bilinear',
                         align_corners= False,
                     ).squeeze(0)
-                    pil = ToPILImage(mode = 'L')(cur_mask)
-                    new_masks.append(pil)
-        return new_masks  
+                    cur_img = F.interpolate(
+                        input = image[idx].unsqueeze(0),
+                        size = (h, w),
+                        mode = 'bilinear',
+                        align_corners = False
+                    ).squeeze(0)
+                    pil_mask = ToPILImage(mode = 'L')(cur_mask)
+                    pil_image = ToPILImage(mode = 'RGB')(cur_img)
+                    new_masks.append(pil_mask)
+                    new_images.append(pil_image)
+        return new_images, new_masks  
 
 
 def main():
     ds = load_dataset('mattmdjaga/human_parsing_dataset', split = 'train')
-    splits = np.arange(1, 1800, 100)
+    splits = np.arange(1, 18, 4)
     mask_output = []
     image_output = []
     for start, end in tqdm(zip(splits[:-1], splits[1:])):
 
         s_ds = ds[start:end]
         dataset = ImageDataset(img_list = s_ds['image'], mask_list= s_ds['mask'])
-        dataloader = DataLoader(dataset, batch_size = 16, shuffle = True, num_workers = 0)
+        dataloader = DataLoader(dataset, batch_size = 4, num_workers = 0)
         
         output_dataset = []
+        image_dataset = []
         model = YoloModify()
         for images, masks, size in dataloader:
-            out_masks = model.modify_mask(images, masks, size, mask_padding = 10)
+            out_images, out_masks = model.modify_mask(images, masks, size, mask_padding = 10)
             output_dataset = out_masks + output_dataset
-        mask_output = output_dataset + mask_output
-        image_output = image_output + s_ds['image']
+            image_dataset  = out_images + image_dataset
+        mask_output = mask_output + output_dataset
+        image_output = image_output + image_dataset
 
     dataset_new = Dataset.from_dict({"image" : image_output, "mask" : mask_output})
+    dataset_new.push_to_hub('YashBhamare123/human_parsing_dataset_plus_neck')
 
 if __name__ == "__main__":
     main()
